ff9283c355e51ec7a401c358a672e1259a46cb8f
ercumentlacin <forever.ducale@gmail.com>
2024-03-10 10:53:30 +0300

    refactor: change application setting file type

---
+++ app.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  expo: {
+    name: "react-native-tmdb-app",
+    slug: "react-native-tmdb-app",
+    version: "1.0.0",
+    extra: {
+      clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+    },
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: ["expo-router", "expo-secure-store"],
+    experiments: {
+      typedRoutes: true,
+    },
+  },
+};
