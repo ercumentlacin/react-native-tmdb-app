4c34665c34ba8bbe03085071c0d78ea7fe92361f
ercumentlacin <forever.ducale@gmail.com>
2024-03-11 09:39:02 +0300

    feat: add redux and store configuration

--- components/layout/root-layout-presentation.tsx
+++ components/layout/root-layout-presentation.tsx
@@ -1,21 +1,25 @@
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import Constants from "expo-constants";
+import { Provider } from "react-redux";
 
 import InitialLayout from "@/components/layout/initial-layout";
 import { useColorScheme } from "@/components/useColorScheme";
 import { tokenCache } from "@/libs/clerk/tokenCache";
+import { store } from "@/redux/store";
 
 export default function RootLayoutPresentation() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <ClerkProvider
-        publishableKey={Constants?.expoConfig?.extra?.clerkPublishableKey}
-        tokenCache={tokenCache}>
-        <InitialLayout />
-      </ClerkProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <ClerkProvider
+          publishableKey={Constants?.expoConfig?.extra?.clerkPublishableKey}
+          tokenCache={tokenCache}>
+          <InitialLayout />
+        </ClerkProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
